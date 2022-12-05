b5ad83e330512f29f69744cc171e87ac3e755c78
Mónica Morales Llamas <monicamoralesllamas@gmail.com>
2022-12-06 00:24:45 +0100

    Añade evento en botón "añadir transación"

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('probando cosas')
+// const createTransaction = document.querySelector("#createTransaction")
+// const transactionConcept = document.querySelector("#conceptTransacation")
+// const trancsactionAmount = document.querySelector("#amountTransaction")
+// console.log(trancsactionAmount,transactionConcept)
+
+// createTransaction.addEventListener("submit", async(event)=> {
+//     let transactionFormData = new FormData(createTransaction)
+
+// // let transaction = {
+// // }
+// })
+
+// function drawTransactions(transaction) {
+//     const transactionConcept = document.querySelector("#conceptTransacation")
+//     const trancsactionAmount = document.querySelector("#amountTransaction")
+
+// }
+
+const form = document.getElementById("formTransaction")
+console.log(form)
+
+form.addEventListener("submit", function(event){
+    event.preventDefault()
+    console.log(event)
+    let transactionFormData = new FormData(form);
+    insertRowInTransactionTable(transactionFormData)
+
+})
+
+
+function insertRowInTransactionTable(transactionFormData){
+    let transactionTableRef = document.getElementById("transactionTable")
+    let newTransactionRowRef = transactionTableRef.insertRow(-1);
+
+    let newTransactionCellRef = newTransactionRowRef.insertCell(0);
+    newTransactionCellRef.textContent = transactionFormData.get("conceptTransacation")
+
+    newTransactionCellRef = newTransactionRowRef.insertCell(1);
+    newTransactionCellRef.textContent = transactionFormData.get("amonuntTransaction")
+
+    transactionFormData.get("conceptTransacation")
+    transactionFormData.get("amonuntTransaction")
+}
+
