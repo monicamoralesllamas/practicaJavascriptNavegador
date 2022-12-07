ff013cef96092398589f1335add6d087a9ed0283
Mónica Morales Llamas <monicamoralesllamas@gmail.com>
2022-12-07 21:47:17 +0100

    Actualiza funciones ingreso y gasto

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const transactionForm = document.querySelector('#formTransaction')
-const concept = document.querySelector('#conceptTransaction')
-const amount = document.querySelector('#amountTransaction')
+const conceptSpace = document.querySelector('#conceptTransaction')
+const amountSpace = document.querySelector('#amountTransaction')
 const historialOfTransactions = document.querySelector('#transactionHistorial')
 const balance = document.querySelector('#totalBalance')
 const totalIncome = document.querySelector('#totalIncome')
@@ -10,90 +10,164 @@ const totalExpense = document.querySelector('#totalExpense')
 const localStorageTransactions = JSON.parse(localStorage.getItem("transactions"));
 let transactions = localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
 
+
 transactionForm.addEventListener("submit", (event) => {
     event.preventDefault();
-
+    
     const transaction = {
-            id: generateIdTransaction(),
-            concept: concept.value, 
-            amount: amount.value,
-        };
+        id: generateIdTransaction(),
+        concept: conceptSpace.value, 
+        amount: amountSpace.value,
+    };
     
     transactions.push(transaction);
-
+    
     drawTransaction(transaction);
-
-    updateValues();
-
-    updateLocalStorage();
     
-    concept.value ="";
-    amount.value = "";
+    // updateIncome(transaction);
+    // updateExpense(transaction);
+    updateTotaltransactions(transaction);
+    updateLocalStorage(transaction);
     
-    });
+    
+    
+    conceptSpace.value ="";
+    amountSpace.value = "";
+    
+});
 
 function drawTransaction(transaction){
-
-
-    const transactionElement = document.createElement("transaction");
-
-    let transactionContent = `
-    <p>${transaction.concept}:           ${transaction.amount}</p>`
-
-    transactionElement.innerHTML=transactionContent;
-    historialOfTransactions.appendChild(transactionElement)
-
-}
-
-function updateValues(){ 
- 
-    const amounts = transactions.map((transaction)=> transaction.amount)
-
-    const total = amounts
-    .reduce((bal, value) => (bal += value),0);
     
+    const transactionSign = transaction.amount <0 ? "-" : "+";
     
-    const income = amounts
-    .filter((value)=> value >0)
-    .reduce((bal,value)=> (bal +=value),0);
+    const transactionElement = document.createElement("list");
+
+    transactionElement.classList.add(transaction.amount <0 ? "minus":"plus");
    
-
-    const expense =  amounts
-    .filter((value)=> value <0)
-    .reduce((bal,value)=> (bal+=value),0 )* (-1);
-
-
-    // totalJson = JSON.stringify(total)
-    // incomeJson = JSON.stringify(income)
-    // expenseJson = JSON.stringify(expense)
-
-    balance.innerText = `${total}€`;
-    totalIncome.innerText = `${income}€`
-    totalExpense.innerText = `${expense}€`;
+    let transactionContent = `${transaction.concept}:${transactionSign}${Math.abs(transaction.amount)}€<button class="delete-button" onclick="deleteTransaction(${
+        transaction.id
+    })">X</button>
+    `
+    transactionElement.innerHTML=transactionContent;
+    
+    historialOfTransactions.appendChild(transactionElement)  
 }
 
 function generateIdTransaction(){
     return Math.floor(Math.random() *100000000);
 }
 
+
+
+function updateIncome(transaction){ 
+      
+let newTransaction = [parseFloat(transaction.amount)]
+let income = 0;
+{for (let i = 0; i < newTransaction.length; i++) {
+    if ((parseFloat(newTransaction)) > 0 ) {    
+    income = + parseFloat(newTransaction)    
+    } else {income = income}}
+
+totalIncome.innerHTML =  `${(parseInt(totalIncome.innerHTML)) + (parseInt(income))}€`
+return income
+}}
+    
+function updateExpense(transaction){
+    
+    let newTransaction = [parseFloat(transaction.amount)]
+
+    let expense = 0;
+    {for (let i = 0; i < newTransaction.length; i++) {
+        if ((parseFloat(newTransaction)) < 0 ) {    
+        expense =  (parseFloat(newTransaction) )   
+        } else {expense = expense}}
+    
+        totalExpense.innerHTML = `${(parseInt(totalExpense.innerHTML)) + (parseInt(expense))}€`
+        return expense
+    }}
+
+
+
+
+function updateTotaltransactions(transaction){
+    updateExpense(transaction)
+    updateIncome(transaction)
+    balance.innerHTML = `${(parseInt(expense) + (parseInt(income)) )}€`
+
+}
+
+
+
+
+    
+
+
+
+// let total = 0
+
+
+//     if ((newTransaction) > 0 ){
+//         income =  (income + newTransaction)
+//         expense = (0) 
+//         total = (income + expense)
+//     } else {
+//         expense = expense + newTransaction
+//         income = 0         
+//         total = expense + income 
+//     }
+   
+
+   
+   
+
+
+// //     const amounts = transactions.map((newTransaction)=> newTransaction.amount)
+    
+// //     total = amounts
+// //     .reduce((bal, value) => (bal += value),0)
+    
+// //     ;
+    
+    
+// //    income = amounts
+// //     .filter((value)=> value >0)
+// //     .reduce((bal,value)=> (bal +=value),0)
+// //     ;
+    
+// //     expense =  amounts
+// //     .filter((value)=> value <0)
+// //     .reduce((bal,value)=> (bal+=value),0 )*-(1)
+    
+    
+    
+//     // totalJson = JSON.stringify(total)
+//     // incomeJson = JSON.stringify(income)
+//     // expenseJson = JSON.stringify(expense)
+    
+//     console.log((total))
+//     console.log(income)
+//     console.log(expense)
+
+//     // balance.textContent =`${total}€`;
+//     // totalIncome.textContent = `${income}€`;
+//     // totalExpense.textContent = `${expense}€`;
+
+
+
+
 function deleteTransaction(id){
-transactions = transactions.filter((transaction) => transaction.id !== id);
-updateLocalStorage();
-
-start();
-
+    transactions = transactions.filter((transaction) => transaction.id !== id);
+    updateLocalStorage();
+    start()
 }
 
 
 function updateLocalStorage() {
     localStorage.setItem("transactions", JSON.stringify(transactions));
-  }
+}
 
 function start(){
     historialOfTransactions.innerHTML = "";
     transactions.forEach(drawTransaction);
-    upDateValues();
+    updateIncome(transaction);
 }
-
-start();
-
---
+++ prueba.js
@@ -0,0 +1,16 @@
+
+let transaction = 50
+
+function updateIncome(transaction){ 
+      
+    let newTransaction = [parseFloat(transaction)]
+    
+    let income = 0;
+    for (let i = 0; i < newTransaction.length; i++) {
+        if ((newTransaction) > 0 );      
+        income =  income + parseInt(newTransaction)    
+        } return console.log(income),console.log(newTransaction.length)
+}
+
+
+updateIncome(50)
