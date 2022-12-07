571a7a6e81c9d5e0fcd6e757b83563026f5d2635
Mónica Morales Llamas <monicamoralesllamas@gmail.com>
2022-12-07 13:42:19 +0100

    Añadimos estilos CSS

--- index.js
+++ index.js
@@ -7,22 +7,25 @@ const balance = document.querySelector('#totalBalance')
 const totalIncome = document.querySelector('#totalIncome')
 const totalExpense = document.querySelector('#totalExpense')
 
+const localStorageTransactions = JSON.parse(localStorage.getItem("transactions"));
+let transactions = localStorage.getItem("transactions") !== null ? localStorageTransactions : [];
 
-transactionForm.addEventListener("submit", async(event) => {
+transactionForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    //INTETO DE SALTAR ERROR EN CASO DE VACIO
-    // if (inputConceptTransacation.value.thrim() === "" || inputAmonuntTransaction.value.thrim() === ""){
-    //     document.querySelector('#error').innerHTML = 
-    //     "<span> Debes introducir una descripción y una cantidad. <span>";
-    // } else {
-    
+
     const transaction = {
+            id: generateIdTransaction(),
             concept: concept.value, 
             amount: amount.value,
         };
     
+    transactions.push(transaction);
+
     drawTransaction(transaction);
-    upDateValues(transaction);
+
+    updateValues();
+
+    updateLocalStorage();
     
     concept.value ="";
     amount.value = "";
@@ -35,45 +38,62 @@ function drawTransaction(transaction){
     const transactionElement = document.createElement("transaction");
 
     let transactionContent = `
-    <p>${transaction.concept} : ${transaction.amount}</p>`
+    <p>${transaction.concept}:           ${transaction.amount}</p>`
 
     transactionElement.innerHTML=transactionContent;
-    historialOfTransactions.prepend(transactionElement)
+    historialOfTransactions.appendChild(transactionElement)
 
 }
 
-function upDateValues(transaction){  
-    let newTransaction = transaction.amount
-    let amounts = amount + (newTransaction)
-    let income
-    let expense
-    let total
+function updateValues(){ 
+ 
+    const amounts = transactions.map((transaction)=> transaction.amount)
 
-    if (newTransaction>0){
-        income = newTransaction
-        total = amounts + income
-    }else{
-        expense = newTransaction 
-        total = amounts + expense
-    }
+    const total = amounts
+    .reduce((bal, value) => (bal += value),0);
     
     
+    const income = amounts
+    .filter((value)=> value >0)
+    .reduce((bal,value)=> (bal +=value),0);
    
 
-    totalJson = JSON.stringify(total)
-    incomeJson = JSON.stringify(income)
-    expenseJson = JSON.stringify(expense)
+    const expense =  amounts
+    .filter((value)=> value <0)
+    .reduce((bal,value)=> (bal+=value),0 )* (-1);
 
-    balance.innerText = `${totalJson}€`;
-    totalIncome.innerText = `${incomeJson}€`
-    totalExpense.innerText = `${expenseJson}€`;
+
+    // totalJson = JSON.stringify(total)
+    // incomeJson = JSON.stringify(income)
+    // expenseJson = JSON.stringify(expense)
+
+    balance.innerText = `${total}€`;
+    totalIncome.innerText = `${income}€`
+    totalExpense.innerText = `${expense}€`;
 }
 
-// function insertIncomeOrWaste(transactionAmount){
-//     const incomeElement = documento.createElement("income")
-//     const wasteElement = document.createElement("waste")
+function generateIdTransaction(){
+    return Math.floor(Math.random() *100000000);
+}
 
-//     if (transactionAmount >0 );
+function deleteTransaction(id){
+transactions = transactions.filter((transaction) => transaction.id !== id);
+updateLocalStorage();
+
+start();
+
+}
 
 
-// }
+function updateLocalStorage() {
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+  }
+
+function start(){
+    historialOfTransactions.innerHTML = "";
+    transactions.forEach(drawTransaction);
+    upDateValues();
+}
+
+start();
+
